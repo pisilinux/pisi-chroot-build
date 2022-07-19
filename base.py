c4f1908d185b691788e308009ddd676f05c85e10
ayhanyalcinsoy <ayhanyalcinsoy@pisilinux.org>
2022-07-19 17:09:40 +0300

    fixed some issues

--- base.py
+++ base.py
@@ -4,8 +4,8 @@ from kayit import *
 import requests
 
 
-BASE = "acl attr baselayout bash bzip2 ca-certificates catbox comar comar-api coreutils cpio cracklib curl db dbus dbus-glib dbus-python diffutils elfutils expat file findutils gdbm gettext glib2 glibc gmp gperftools grep gzip kernel-headers less leveldb libcap libcap-ng libffi libgcc libgomp libidn2 libpcre libsigsegv libssh2 libunistring libunwind libuser libxml2 mudur nasm ncurses openssl pam patch perl piksemel pisi pisilinux-python plyvel popt procps pycurl python python-pyliblzma readline run-parts sed shadow snappy sqlite tar unzip urlgrabber which xz zip zlib".split()
-DEVEL = "autoconf autogen automake binutils bison flex gawk gc gcc gmp gnuconfig guile libmpc libtool-ltdl libtool lzo m4 make mpfr pkgconfig python-iniparse yacc glibc-devel".split()
+BASE = "acl attr baselayout bash bzip2 ca-certificates catbox comar comar-api coreutils cpio cracklib curl db dbus dbus-glib dbus-python diffutils elfutils expat file findutils gdbm gettext glib2 glibc gmp gperftools grep gzip kernel-headers less leveldb libcap libcap-ng libffi libgcc libgomp libidn2 libpcre libssh2 libunistring libunwind libuser libxml2 mudur ncurses openssl pam patch perl piksemel pisi pisilinux-python plyvel popt procps pycurl python python-pyliblzma readline run-parts sed shadow snappy sqlite tar unzip urlgrabber which xz zip zlib python-six polkit audit libnsl libtirpc libnghttp2 openldap-client zstd".split()
+DEVEL = "autoconf autogen automake binutils bison flex gawk gc gcc gmp gnuconfig guile libmpc libtool-ltdl libtool libsigsegv lzo m4 make mpfr nasm pkgconfig python-iniparse yacc glibc-devel".split()
 CACHE = "paket"
 
 
@@ -334,7 +334,7 @@ class Chroot:
         self.runCommand("mknod -m 666 /dev/zero c 1 5")
         self.runCommand("mknod -m 666 /dev/random c 1 8")
         self.runCommand("mknod -m 666 /dev/urandom c 1 9")
-        self.runCommand("mkdir -m 1777 /dev/shm")
+        #self.runCommand("mkdir -m 1777 /dev/shm")
         self.runCommand("mknod -m 666 /dev/tty c 5 0")
         self.runCommand("mknod -m 600 /dev/console c 5 1")
         self.runCommand("mknod -m 666 /dev/tty0 c 5 0")
